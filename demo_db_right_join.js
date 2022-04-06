1c6324dc9ab1eeb00853781ae4ef0ae5ab8089f0
Annely Kurg <annely.kurg@khk.ee>
2022-04-06 19:52:19 +0300

    Left join and right join

---
+++ demo_db_right_join.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "qwerty",
+    database: "mydb"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    var sql = "SELECT customers.name AS user_name, products.name AS favorite FROM customers RIGHT JOIN products ON customers.favourite_product = products.product_id";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+});
